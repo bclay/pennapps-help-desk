71db75cd85fd145580b14f8d16b019e5b588015b
Brynn Claypoole <bclay@seas.upenn.edu>
2014-06-14 16:56:49 -0400

    fixed bug, added exports back

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,11 @@
 function start(response){
 	console.log("Request handler 'start' was called.");
-	var content = "Hello hackers! Welcome to the PennApps Help Desk!";
-
+	var content = "Hello hacker! Welcome to the PennApps Help Desk!";
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write(content);
 	response.end();
 
 	return content;
 }
+
+exports.start = start;
