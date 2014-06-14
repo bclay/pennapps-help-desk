eca309247720fb310419b5cc5182cf569f93794b
Brynn Claypoole <bclay@seas.upenn.edu>
2014-06-14 16:45:16 -0400

    some basic code that does not work

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start(response){
+	console.log("Request handler 'start' was called.");
+	var content = "Hello hackers! Welcome to the PennApps Help Desk!";
+
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write(content);
+	response.end();
+
+	return content;
+}
