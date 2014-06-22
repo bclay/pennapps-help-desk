487d0e0d2bc65d387287cdba241707eaf6d209f8
Brynn Claypoole <bclay@seas.upenn.edu>
2014-06-22 16:55:11 -0400

    Trying to get this thing online

---
+++ models/loader.js
@@ -0,0 +1,11 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath(../../config.json);
+var simpledb = new AWS.SimpleDB();
+var async = require('async');
+
+var newQ = [{question:'What\'s in a name?', answer:'That which we call a rose by any other name would smell as sweet.'}];
+simpledb.createDomain({DomainName:'helpdeskqs'}, function(err, data){
+	if(err){
+		
+	}
+});
