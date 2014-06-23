b320d079d7de818875be87034348334ffdcc87a9
Brynn Claypoole <bclay@seas.upenn.edu>
2014-06-23 11:33:07 -0400

    added logfmt, Procfile, and others to prepare for Heroku

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var logfmt = require("logfmt");
+var router = require("./routes/router.js");
+
+app.use(logfmt.requestLogger());
+
+app.get('/', router.launch);
+
+
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function(){
+	console.log("Listening on " + port);
+});
