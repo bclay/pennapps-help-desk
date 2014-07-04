d7365ceef293ce18a337c655ebff5def3b693b50
Brynn Claypoole <bclay@seas.upenn.edu>
2014-07-04 15:43:19 -0400

    brynn css and initial loader

--- models/loader.js
+++ models/loader.js
@@ -1,4 +1,5 @@
-/* Don't mind me, I'm a work in progress.
+//not sure that the db is set up in the best way
+//talk about db setup
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath(../../config.json);
 var simpledb = new AWS.SimpleDB();
@@ -7,7 +8,18 @@ var async = require('async');
 var newQ = [{question:'What\'s in a name?', answer:'That which we call a rose by any other name would smell as sweet.'}];
 simpledb.createDomain({DomainName:'helpdeskqs'}, function(err, data){
 	if(err){
-		
+		console.log("Cannot create: "+err);
+    } else {
+        async.forEach(helpdeskqs, function(w, callback) {
+        simpledb.putAttributes({DomainName:'helpdeskqs', ItemName:w.question,
+            Attributes: [{Name:'answer', Value:w.answer}]},
+            function(err, data) {
+                if (err)
+                	console.log("Cannot put: "+err);
+                callback();
+              });
+          });
+        }
 	}
 });
 */
